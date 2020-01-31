af42280be349746b12c2511ce3a4c72230be180a
Justin Kaunitz <justin.kaunitz@gmail.com>
2020-01-30 17:03:59 -0800

    changed from input to textarea in description

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -85,17 +85,19 @@ export default class PortfolioForm extends Component {
                             onChange={this.handleChange}
                         />
                         
-                        <input
-                            type='text'
+                        <select
                             name='category'
-                            placeholder='Category'
                             value={this.state.category}
                             onChange={this.handleChange}
-                        />
+                        >
+                            <option value ='eCommerce'>eCommerce</option>
+                            <option value ='Scheduling'>Scheduling</option>
+                            <option value ='Enterprise'>Enterprise</option>
+                        </select>
                     </div>
 
                     <div>
-                        <input
+                        <textarea
                             type='text'
                             name='description'
                             placeholder='Description'
