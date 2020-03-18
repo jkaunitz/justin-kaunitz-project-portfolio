47683a3e7e383f0da9bb205d65fc36ac50c492ce
Justin Kaunitz <justin.kaunitz@gmail.com>
2020-03-18 14:09:52 -0700

    The blog API is going well and involves in storing data

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function() {
-    return (
-        <div>
-            <h2>Blog</h2>
+class Blog extends Component {
+    constructor() {
+        super();
 
+        this.state = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
+    }
+
+    getBlogItems() {
+        axios.get("https://jkaunitz.devcamp.space/portfolio/portfolio_blogs", { 
+            withCredentials: true 
+        }).then(response => {
+            this.setState({
+                blogItems: response.data.portfolio_blogs
+            })
+        }).catch(error => {
+            console.log('getBlogItems error', error);
+        })
+    }
+
+    componentWillMount() {
+        this.getBlogItems(); 
+    }
+
+    render() {
+        return (
             <div>
-                <Link to='/about-me'>My personal information</Link>
+                <h2>Blog</h2>
+    
+                <div>
+                    <Link to='/about-me'>My personal information</Link>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Blog;
