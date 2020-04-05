4d2ae8894d137213898b6fda69748be70bc5b580
Justin Kaunitz <justin.kaunitz@gmail.com>
2020-04-05 15:03:55 -0700

    Styled modal

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -48,29 +48,31 @@ export default class BlogForm extends Component {
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value
-        })
+        });
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input 
-                    type='text'
-                    onChange={this.handleChange} 
-                    name='title'
-                    placeholder='Blog Title'
-                    value={this.state.title}
-                />
+            <form onSubmit={this.handleSubmit} className='blog-form-wrapper'>
+                <div className='two-column'>
+                    <input 
+                        type='text'
+                        onChange={this.handleChange} 
+                        name='title'
+                        placeholder='Blog Title'
+                        value={this.state.title}
+                    />
 
-                <input 
-                    type='text'
-                    onChange={this.handleChange} 
-                    name='blog_status'
-                    placeholder='Blog Status'
-                    value={this.state.blog_status}
-                />
+                    <input 
+                        type='text'
+                        onChange={this.handleChange} 
+                        name='blog_status'
+                        placeholder='Blog Status'
+                        value={this.state.blog_status}
+                    />
+                </div>
 
-                <button>Save</button>
+                <button className='btn'>Save</button>
             </form>
         );
     }
